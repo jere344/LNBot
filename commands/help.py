46e17103c27c61ef7764c0dcd2def36289b4cb88
jeremy <jere344@pop-os.localdomain>
2022-04-01 12:04:54 +0200

    help message will change depending on the prefix in config

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,6 @@
 import lib
 from lnbotdecorator import LnBotDecorator
+import config
 
 
 @lib.bot.command()
@@ -10,12 +11,12 @@ async def help(ctx, commande=None):
             ctx.send("Cette commande n'existe pas")
             return
         await ctx.send(
-            f"```\n.{lib.dict_of_help_command[commande].exemple}\n>>> {lib.dict_of_help_command[commande].message}\n```"
+            f"```\n{config.command_prefix}{lib.dict_of_help_command[commande].exemple}\n>>> {lib.dict_of_help_command[commande].message}\n```"
         )
     else:
         help_message = "\n".join(
             [
-                f"{key:20s} .{value.exemple}\n"
+                f"{key:20s} {config.command_prefix}{value.exemple}\n"
                 for key, value in lib.dict_of_help_command.items()
             ]
         )
